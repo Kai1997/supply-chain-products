fa30dfb0fa7953415dd7e916684cb7ebb546b397
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-02-12 13:57:17 +0700

    join proxy ownable to admin

--- Admin.sol
+++ Admin.sol
@@ -4,52 +4,71 @@ pragma solidity ^0.4.25;
 /**
  * @title Admin
  */
- import './Ownable.sol';
- 
-contract Admins is Ownable{
-    mapping (address => bool) admins;
 
+interface OwnableInterface {
+    function isOwner() external view returns (bool);
+}
+
+contract Admins {
+    mapping (address => bool) admins;
     
+    OwnableInterface public OwnableContract;
+    
+    constructor(address _contract) public {
+      OwnableContract = OwnableInterface(_contract);
+    }
+    
+    modifier roleOwner() {
+        require(OwnableContract.isOwner(),"Account not is owner");
+        _;
+    }
+    
+    modifier validateAccount(address _account) {
+        require(_account != address(0),"Account not exist");
+        require(!has(_account),"Account not register");
+        _;
+    }
   /**
    * @dev give an account access to this role
    */
-  function addAdmin(address _account) public onlyOwner {
-    require(_account != address(0),"Account not exist");
-    require(!has(_account),"Account not register");
-
+    function joinNetwork(address _contract)
+        public
+    {
+        OwnableContract = OwnableInterface(_contract);
+    
+    }
+  function addAdmin(address _account) public roleOwner validateAccount(_account) {
     admins[_account] = true;
-   
   }
 
   /**
    * @dev remove an account's access to this role
    */
-  function removeAdmin(address account) onlyOwner public {
-    require(account != address(0),"Account not exist");
-    require(has(account),"Account not register");
-    
-    admins[account] = false;
+  function removeAdmin(address _account) roleOwner validateAccount(_account) public {
+    admins[_account] = false;
   }
-    function isAdmin(address account)
+  
+  function isAdmin(address _account)
     public
     view
     returns (bool)
   {
     
-    return has(account);
+    return has(_account);
   }
   /**
    * @dev check if an account has this role
    * @return bool
    */
-  function has(address account)
+   
+  function has(address _account)
     internal
     view
     returns (bool)
   {
-    require(account != address(0),"Account not exist");
+    require(_account != address(0),"Account not exist");
     
-    return admins[account] == true;
+    return admins[_account] == true;
   }
   
   
