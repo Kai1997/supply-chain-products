bb4796136122b67f6cba43b39794c3b7870a290f
Kai1997 <nguyendinhkhai2111997@gmail.com>
2020-02-12 15:57:55 +0700

    remove all import

--- 3PLRole.sol
+++ 3PLRole.sol
@@ -1,7 +1,93 @@
 pragma solidity ^0.4.24;
 
-// Import the library 'Roles'
-import "./Roles.sol";
+library Roles {
+    struct Role {
+        mapping (address => User) bearer;
+    }
+    struct User {
+        address account;
+        bool status;
+        string name;
+        string identify;
+        string company;
+        string lati;
+        string longt;
+    }
+    
+  /**
+   * @dev give an account access to this role
+   */
+  function add(Role storage role, address _account, string memory  _name, string memory _company,string memory _identify, string memory _lati, string memory _longt) internal {
+    require(_account != address(0),"Account not exist");
+    require(!has(role, _account),"Account not register");
+
+    role.bearer[_account] = User({
+        account: _account,
+        status: true,
+        name : _name,
+        company : _company,
+        identify : _identify,
+        lati : _lati,
+        longt : _longt
+    });
+   
+  }
+
+  /**
+   * @dev remove an account's access to this role
+   */
+  function remove(Role storage _role, address _account) internal {
+    require(_account != address(0),"Account not exist");
+    require(has(_role, _account),"Account not register");
+    
+    _role.bearer[_account].status = false;
+  }
+
+  /**
+   * @dev check if an account has this role
+   * @return bool
+   */
+  function has(Role storage _role, address _account)
+    internal
+    view
+    returns (bool)
+  {
+    require(_account != address(0),"Account not exist");
+    
+    return _role.bearer[_account].status == true;
+  }
+    /**
+    * @dev get infomation user
+    */
+    function get(
+        Role storage _role, 
+        address _account
+    ) 
+    internal 
+    view 
+    returns (
+      address, 
+      string, 
+      string, 
+      string,
+      string, 
+      string
+      )
+    {
+    require(_account != address(0),"Account not exist");
+    require(has(_role, _account),"Account not register");
+    
+    return (
+        _role.bearer[_account].account,
+        _role.bearer[_account].name, 
+        _role.bearer[_account].company,
+        _role.bearer[_account].identify,
+        _role.bearer[_account].lati, 
+        _role.bearer[_account].longt
+        );
+  }
+
+}
 
 interface AdminInterface {
     function isAdmin(address _account) view external returns(bool);
